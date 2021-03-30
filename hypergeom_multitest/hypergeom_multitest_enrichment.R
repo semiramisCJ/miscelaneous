73805e4d34831875195fbad4a3a7470fe9c70bd7
semiramisCJ <semiramis.cj@gmail.com>
2021-03-29 22:28:54 -0600

    Use more significative names

--- hypergeom_multitest/hypergeom_multitest_enrichment.R
+++ hypergeom_multitest/hypergeom_multitest_enrichment.R
@@ -1,55 +1,71 @@
-testEnrichment<-function(geneCounts, idIndex, qIndex, kIndex, mIndex, nIndex, 
-                       descIndex, multitestCorrection="", cutoff=0.05){
+testEnrichment<-function(go_sample_counts, multitest_correction="", cutoff=0.05, verbose_output=TRUE){
+  
   # Multi-test correction methods available:
   # "holm", "hochberg", "hommel", "bonferroni", "BH", "BY",
   # "fdr"
-  # q: hitInSample - The GO term of interest
-  # m: hitInPop - How many times the GO of interest appears in the entire dataset
-  # n: failInPop - How many GOs (not of interest) appear in the entire dataset
-  # k: sampleSize - How many GOs are there in total in the category
+  # q: hitInSample - The GO term of interest #go_of_interest_in_sample
+  # m: hitInPop - How many times the GO of interest appears in the entire dataset #go_of_interest_in_pop
+  # n: failInPop - How many GOs (not of interest) appear in the entire dataset #other_gos_in_sample
+  # k: sampleSize - How many GOs are there in total in the category #total_gos_in_sample
   
-  # geneCounts - The dataset
+  # go_sample_counts - The dataset
   # *Index - The column number with the parameter
   # id* - The category name (HGT/nonHGT)
   # desc* - The description, for example, the GO name
   
-  # multitestCorrection - Multi-test correction
+  # multitest_correction - Multi-test correction
   # cutoff - Minimum accepted p-value
 
   # The note will be of the 
   # "annotSpot(annotGenome)/spotSize"
   
-  res<-matrix(ncol = 6, nrow = nrow(geneCounts))
-  colnames(res) <- c("ID","pval","adjPval","description", "note", "signif")
-  for (i in seq_len(nrow(geneCounts))) {
-    ID<-as.character(geneCounts[i,idIndex])
-    q<-geneCounts[i,qIndex]
-    k<-geneCounts[i,kIndex]
-    m<-geneCounts[i,mIndex]
-    n<-geneCounts[i,nIndex]
-    description<-as.character(geneCounts[i,descIndex])
-    pval<-phyper(q-1, m, n, k, lower.tail = FALSE) #p(X>=q)
-    note<-paste0(q,'(',m,')','/',k)
-    adjPval <- pval
-    res[i,] <- c(ID,pval,adjPval,description, note, "")
+  mandatory_columns <- c("goterm_id", "sample_id",
+                         "go_of_interest_in_sample", "go_of_interest_in_pop",
+                         "other_gos_in_sample", "total_gos_in_sample")
+  if(!all(mandatory_columns %in% colnames(go_sample_counts))){
+    stop(paste("The input dataframe must have all of the following columns:",
+         mandatory_columns)
+         )
   }
   
-  if(multitestCorrection != ""){ 
-    res[,3]<-p.adjust(res[,2], "fdr")
+  max_size <- nrow(go_sample_counts)
+  res <- data.frame("goterm_id" = character(max_size),
+                    "pvalue" = numeric(max_size),
+                    "adj_pvalue" = numeric(max_size),
+                    "sample_id" = character(max_size),
+                    "note" = character(max_size),
+                    "p_less_than_cutoff" = character(max_size))
+
+  for (i in seq_len(nrow(go_sample_counts))) {
+    res[i, "goterm_id"] <- as.character(go_sample_counts[i, "goterm_id"])
+    res[i, "sample_id"] <- go_sample_counts[i, "sample_id"]
+    q <- go_sample_counts[i, "go_of_interest_in_sample"]
+    k <- go_sample_counts[i, "total_gos_in_sample"]
+    m <- go_sample_counts[i, "go_of_interest_in_pop"]
+    n <- go_sample_counts[i, "other_gos_in_sample"]
+    res[i, "pvalue"] <- phyper(q - 1, m, n, k, lower.tail = FALSE) # p(X >= q)
+    res[i, "note"] <- paste0(q,'(',m,')','/',k)
+    res[i, "adj_pvalue"] <- pval
   }
   
-  for (i in seq_len(nrow(geneCounts))) {
+  if(multitest_correction != ""){ 
+    res[, "adj_pvalue"] <- p.adjust(res[, "pvalue"], "fdr")
+  }
+  
+  for (i in seq_len(nrow(go_sample_counts))) {
     if(adjPval < cutoff){
-      res[i, 6] <- "YES"
+      res[i, "note"] <- "YES"
     }
   }
   return(res)
 }
 
-geneCounts <- read.csv("testdata.txt", sep = "\t")
-head(geneCounts)
+go_sample_counts <- read.csv("testdata.txt", sep = "\t", header = FALSE)
+colnames(go_sample_counts) <- c("sample_id", "go_of_interest_in_sample","total_gos_in_sample",
+                                "go_of_interest_in_pop", "other_gos_in_sample", "goterm_id")
+head(go_sample_counts)
 
 # Bonferroni: The pvalues are multiplied by the number of comparisons
-result <- testEnrichment(geneCounts, 1, 2, 3, 4, 5, 6, "bonferroni")
+result <- testEnrichment(go_sample_counts, "bonferroni")
 head(result)
 
