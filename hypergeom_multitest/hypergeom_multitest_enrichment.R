f2850047fd431ac9d94eec520c13ebb65ba38831
semiramisCJ <semiramis.cj@gmail.com>
2021-03-29 22:56:49 -0600

    Added documentation

--- hypergeom_multitest/hypergeom_multitest_enrichment.R
+++ hypergeom_multitest/hypergeom_multitest_enrichment.R
@@ -1,28 +1,45 @@
-testEnrichment<-function(go_sample_counts, multitest_correction="", cutoff=0.05, verbose_output=TRUE){
-  
-  # Multi-test correction methods available:
-  # "holm", "hochberg", "hommel", "bonferroni", "BH", "BY",
-  # "fdr"
+#' Test for enrichment in a dataset with the hypergeometric test
+#'
+#' @param go_sample_counts, data.frame with the following columns:
+#' "goterm_id", character - Either the identifier or description of the GO term of interest to test for enrichment in sample.
+#' "sample_id", character - The sample identifier.
+#' "go_of_interest_in_sample", integer - Number of hits of the GO term of interest in the sample (q parameter in phyper), 'hit in sample'.
+#' "go_of_interest_in_pop" - How many times the GO term of interest appears in the entire dataset (m parameter in phyper), 'hit in population'.
+#' "other_gos_in_pop" - How many other GO terms (those not of interest) appear in the entire dataset (n parameter in phyper), 'fail in population'.
+#' "total_gos_in_sample" - How many GOs are there in total in the sample (k parameter in phyper), sample size.
+#'  
+#' @param multitest_correction, character, either empty or one of the options of stats::p.adjust
+#' "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"
+#' @param cutoff, numeric, default: 0.05 
+#' @param verbose_output, bool, whether to store or not non-significative results
+#'
+#' @return data.frame with the following columns:
+#' "goterm_id", character - Either the identifier or description of the GO term of interest to test for enrichment in sample.
+#' "pvalue" - numeric
+#' "adj_pvalue" - numeric
+#' "sample_id", character - The sample identifier.#' "pvalue" -
+#' "note", character - The numbers that respond to the wording 
+#' 'There are k hits of the GO of interest in the sample (out of m hits of that GO term in the entire dataset) / out of a grand total of n GO terms annotated in the entire dataset'
+#' "p_less_than_cutoff" - YES / NO (whether the adjusted pvalue < cuttoff)
+#' @export
+#'
+#' \dontrun{
+#' @examples
+#' go_sample_counts <- read.csv("testdata.txt", sep = "\t", header = FALSE)
+#' colnames(go_sample_counts) <- c("sample_id", "go_of_interest_in_sample","total_gos_in_sample", "go_of_interest_in_pop", "other_gos_in_pop", "goterm_id")
+#' Bonferroni: The pvalues are multiplied by the number of comparisons
+#' result <- test_enrichment_hypergeom(go_sample_counts, "bonferroni")
+#' }
+test_enrichment_hypergeom <- function(go_sample_counts, multitest_correction="", cutoff=0.05, verbose_output=TRUE){
   # q: hitInSample - The GO term of interest #go_of_interest_in_sample
   # m: hitInPop - How many times the GO of interest appears in the entire dataset #go_of_interest_in_pop
-  # n: failInPop - How many GOs (not of interest) appear in the entire dataset #other_gos_in_sample
+  # n: failInPop - How many GOs (not of interest) appear in the entire dataset #other_gos_in_pop
   # k: sampleSize - How many GOs are there in total in the category #total_gos_in_sample
-  
-  # go_sample_counts - The dataset
-  # *Index - The column number with the parameter
-  # id* - The category name (HGT/nonHGT)
-  # desc* - The description, for example, the GO name
-  
-  # multitest_correction - Multi-test correction
-  # cutoff - Minimum accepted p-value
-
-  # The note will be of the 
-  # "annotSpot(annotGenome)/spotSize"
   
   mandatory_columns <- c("goterm_id", "sample_id",
                          "go_of_interest_in_sample", "go_of_interest_in_pop",
-                         "other_gos_in_sample", "total_gos_in_sample")
-  if(!all(mandatory_columns %in% colnames(go_sample_counts))){
+                         "other_gos_in_pop", "total_gos_in_sample")
+  if (!all(mandatory_columns %in% colnames(go_sample_counts))) {
     stop(paste("The input dataframe must have all of the following columns:",
          mandatory_columns)
          )
@@ -42,13 +59,13 @@ testEnrichment<-function(go_sample_counts, multitest_correction="", cutoff=0.05,
     q <- go_sample_counts[i, "go_of_interest_in_sample"]
     k <- go_sample_counts[i, "total_gos_in_sample"]
     m <- go_sample_counts[i, "go_of_interest_in_pop"]
-    n <- go_sample_counts[i, "other_gos_in_sample"]
+    n <- go_sample_counts[i, "other_gos_in_pop"]
     res[i, "pvalue"] <- phyper(q - 1, m, n, k, lower.tail = FALSE) # p(X >= q)
     res[i, "note"] <- paste0(q,'(',m,')','/',k)
     res[i, "adj_pvalue"] <- pval
   }
   
-  if(multitest_correction != ""){ 
+  if (multitest_correction != "") { 
     res[, "adj_pvalue"] <- p.adjust(res[, "pvalue"], "fdr")
   }
   
@@ -59,13 +76,4 @@ testEnrichment<-function(go_sample_counts, multitest_correction="", cutoff=0.05,
   }
   return(res)
 }
-
-go_sample_counts <- read.csv("testdata.txt", sep = "\t", header = FALSE)
-colnames(go_sample_counts) <- c("sample_id", "go_of_interest_in_sample","total_gos_in_sample",
-                                "go_of_interest_in_pop", "other_gos_in_sample", "goterm_id")
-head(go_sample_counts)
-
-# Bonferroni: The pvalues are multiplied by the number of comparisons
-result <- testEnrichment(go_sample_counts, "bonferroni")
-head(result)
 
